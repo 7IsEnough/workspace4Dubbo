aa41b4bcac3742a55ac10ac1367c9a7fad40cebf
promise <976949689@qq.com>
2025-02-07 00:40:17 +0800

    2025年2月7日00:40:15

--- dubbo-pro/dubbo-service/src/main/java/com/clearlove/service/impl/UserServiceImpl.java
+++ dubbo-pro/dubbo-service/src/main/java/com/clearlove/service/impl/UserServiceImpl.java
@@ -1,16 +1,18 @@
 package com.clearlove.service.impl;
 
 import com.clearlove.service.UserService;
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.Service;
 
 /**
  * @author promise
  * @date 2025/2/6 - 0:39
  */
-@Service
+// @Service Spring IOC 容器的注解
+
+@Service // 将这个类提供的方法（服务）对外发布。将访问的地址 ip，端口，服务名，版本号，分组等信息发布到注册中心上
 public class UserServiceImpl implements UserService {
 
   public String sayHello() {
-    return "hello dubbo!";
+    return "hello dubbo from dubbo-service!";
   }
 }
--- dubbo-pro/dubbo-web/src/main/java/com/clearlove/controller/UserController.java
+++ dubbo-pro/dubbo-web/src/main/java/com/clearlove/controller/UserController.java
@@ -2,6 +2,7 @@ package com.clearlove.controller;
 
 import com.clearlove.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-  private final UserService userService;
+  /*
+    1. 从zookeeper注册中心获取userService的访问Url
+    2. 进行远程调用RPC
+    3. 将结果封装为一个代理对象。给变量赋值
+   */
+  @Reference // dubbo 远程注入
+  private UserService userService;
 
   @RequestMapping("/sayHello")
   public String sayHello(){
